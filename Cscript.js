0d64a160a6a6ac12e29a2e1999bf37201a9f1307
danthamGowthami <76723802+danthamGowthami@users.noreply.github.com>
2021-07-12 10:39:56 +0530

    Add files via upload

---
+++ Cscript.js
@@ -0,0 +1,97 @@
+console.log("hello welcome to b.tech departments")
+
+
+
+
+
+
+
+let cse1 = document.querySelector('#cse');
+let ece1 = document.querySelector('#ece');
+let mech1 = document.querySelector('#mech');
+
+let civil1 = document.querySelector('#civil');
+let eee1 = document.querySelector('#eee');
+
+
+
+cse1.addEventListener('click', () => {
+  document.body.style.backgroundImage = "url('CSE.jpg')"});
+
+ece1.addEventListener('click', () => {
+  document.body.style.backgroundImage = "url('Ece1.jpg')"
+});
+
+mech1.addEventListener('click', () => {
+  document.body.style.backgroundImage = "url('Mech.jpg')"
+});
+
+
+civil1.addEventListener('click', () => {
+  document.body.style.backgroundImage = "url('Civil.jpg')"
+});
+
+eee1.addEventListener('click', () => {
+  document.body.style.backgroundImage = "url('Eee.jpg')"
+});
+function Cse()
+{
+  document.getElementById("Cse").innerHTML=
+  "Scope After Computer Science Engineering in India:"+"<br><br>"+ "CSE or computer Science Engineering has emerged as a very popular educational course. This course integrates the key elements of computer programming and networking and several fields of engineering. Aspirants for this course gain knowledge and develop ideas on the design, implementation, and management of the information system to develop computer hardware and software. This course is offered to students at the undergraduate as well as the postgraduate levels and students obtain the B.Tech and M.Tech degrees respectively"
+  
+  
+  
+  
+}
+function Ece()
+{
+  document.getElementById("Ece").innerHTML = "<b><big>Department Of Electrical & Electronics Engineering</big></b>"+"<br><br>"+"Department of Electrical & Electronics Engineering was started in 2008 with an intake of 120 students in Under Graduate Program (B.Tech) with the approval of AICTE and is currently affiliated to JNTUA Anantapuramu, Andhra Pradesh. The intake is increased to 180. The Department will be the center of Excellence with competent facility State-of-Art laboratory equipment, adequate infrastructure. The department soon won laurels with the dedication and devotion of the faculty who are well qualified and rich experienced.";
+
+}
+function Mech()
+{
+  document.getElementById("Mech").innerHTML = "<b><big>Departments Of Mechanical Engineering </big></b>"+"<br><br>"+"Department of Mechanical Engineering was started in 2008 with an intake of 120 students in Under Graduate Program (B.Tech) with the approval of AICTE and is currently affiliated to JNTUA Anantapuramu, Andhra Pradesh. The intake is increased to 180. The Department will be the center of Excellence with competent facility State-of-Art laboratory equipment, adequate infrastructure. The department soon won laurels with the dedication and devotion of the faculty who are well qualified and rich experienced.loja virtual grati";
+
+}
+function Civil()
+{
+  document.getElementById("Civil").innerHTML = "<b><big>.  Department Of Civil Engineering</big></b>"+"<br><br>"+" The Department also offers an Post Graduate programme (M.Tech) in Civil Engineering, started from 2012. The department lays stress on all round development of the students as well as faculty and laboratory staff by providing them a tech friendly environment and necessary infrastructure for carrying out academic work.criar loja virtual   ";
+
+}
+function Eee()
+{
+  document.getElementById("Eee").innerHTML = ".       The Electrical & Electronics Engineering department is committed to continuous technical improvement of its educational environment in order to provide quality education and professional experiences to empower students in the computing disciplines to attain their professional and academic goals.meuip.co   ";
+
+}
+function Mcse()
+{
+  document.getElementById("Mcse").innerHTML=
+  "Scope After Computer Science Engineering in India:"+"<br><br>"+ "CSE or computer Science Engineering has emerged as a very popular educational course. This course integrates the key elements of computer programming and networking and several fields of engineering. Aspirants for this course gain knowledge and develop ideas on the design, implementation, and management of the information system to develop computer hardware and software. This course is offered to students at the undergraduate as well as the postgraduate levels and students obtain the B.Tech and M.Tech degrees respectively"+"<p></p>"+
+"The modern day technology has transformed the world into a global village, and computers and the internet are an integral part of it. Computer Science Engineering therefore, has become a very sought after course, with excellent scope"
+  
+  
+  
+  
+}
+function Mece()
+{
+  document.getElementById("Mece").innerHTML = "<b><big>Department Of Electrical & Electronics Engineering</big></b>"+"<br><br>"+"Department of Electrical & Electronics Engineering was started in 2008 with an intake of 120 students in Under Graduate Program (B.Tech) with the approval of AICTE and is currently affiliated to JNTUA Anantapuramu, Andhra Pradesh. The intake is increased to 180. The Department will be the center of Excellence with competent facility State-of-Art laboratory equipment, adequate infrastructure. The department soon won laurels with the dedication and devotion of the faculty who are well qualified and rich experienced.";
+    
+
+}
+function Mmech()
+{
+  document.getElementById("Mmech").innerHTMLb="<big>Departments Of Mechanical Engineering </big></b>"+"<br><br>"+"Department of Mechanical Engineering was started in 2008 with an intake of 120 students in Under Graduate Program (B.Tech) with the approval of AICTE and is currently affiliated to JNTUA Anantapuramu, Andhra Pradesh. The intake is increased to 180. The Department will be the center of Excellence with competent facility State-of-Art laboratory equipment, adequate infrastructure. The department soon won laurels with the dedication and devotion of the faculty who are well qualified and rich experienced.loja virtual grati";
+
+
+}
+function Mcivil()
+{
+  document.getElementById("Mcivil").innerHTML =     "<b><big>.  Department Of Civil Engineering</big></b>"+"<br><br>"+" The Department also offers an Post Graduate programme (M.Tech) in Civil Engineering, started from 2012. The department lays stress on all round development of the students as well as faculty and laboratory staff by providing them a tech friendly environment and necessary infrastructure for carrying out academic work.";
+
+}
+function Meee()
+{
+  document.getElementById("Meee").innerHTML = " <b><big> Department Of Electrical & Electronics Engineering </big></b> The Electrical & Electronics Engineering department is committed to continuous technical improvement of its educational environment in order to provide quality education and professional experiences to empower students in the computing disciplines to attain their professional and academic goals.meuip.co  ";
+
+}
---
+++ Trans.js
@@ -0,0 +1,58 @@
+
+
+var a;
+
+function Transport()
+{
+  document.getElementById("demo").innerHTML
+  
+  
+ = "<u>Duties of Transport Division:</u>"+"<br><br>"+"1. Allocation of bus routes for the for students "+"<br><br>"+"2.Supervising the daily bus operation and giving instructions to the transport supervisor"+"<br><br>"+"3.Inspecting the condition of the buses and reporting for necessary actions"+"<br><br>"+"4.Issuing Bus passes"+"<br><br>"+"5.Periodically checking the documents of the buses (College & Contract)"+"<br><br>"+"6. Verifying and passing the bill for making payment Monitoring Director's van operation"+"<br><br>"+"7.Reporting to the superior as and when required.como criar uma loja virtual"
+ 
+/*window.addEventListener('load', function(){
+   document.querySelector('input[type="file"]').addEventListener('change',function()
+   {
+     var img = document.createElement("img");
+     img.src = "Vans.jpg";
+     var src = document.getElementById("#image");
+     img.onload=imgLoaded;
+   })
+ }
+ function imgLoaded(e){
+   alert(e);
+ }*/
+  
+ 
+ 
+}
+function Placements()
+{
+  var x=document.getElementById("demo1").innerHTML="<big><b>Dr. A. BABU NAIDU</b></big>"+"<br><br>"+
+  "Training and Placement Officer"+"<br><br>"+"<strong>Objectives</strong>"+"<br><br>"+"1.   To organize campus interviews for final year students with industries and business houses of repute from all over India. "+"<br><br>"
+  +"2.  To prepare students to face campus interviews by arranging training in Aptitude tests, group discussions, preparing for Technical and HR interviews through professional trainers."+"<br><br>"+"3.  To arrange Industrial visits and In-Plant training. To arrange final semester project for the students.  "+"<br><br>"+"4.  To promote career counseling by organising guidance lectures by senior corporate personnel and most importantly by the immediately placed senior students"
+  
+  
+  
+  if(x.style.display=="none")
+  {
+  x.style.display="block";
+  }
+  else{
+    x.style.display="none";
+  }
+  
+}
+function Hostels()
+{
+  var x=document.getElementById("demo2").innerHTML ="<big><strong>Hostels:</strong></big>"+"<br><br>"+"A hostel for girl and boys students with all amenities is available in the premises of the college as well as Pileru town.The occupancy of hostel rooms is restricted to two to three students for each room. Every room is provided with an attached Bath Room, Cots with Dunlop Beds Pillows, Dressing Table. Generator and Magazines for leisurely study and entertainment are also available. Faculty members are appointed as wardens who monitor the facilities, hygiene, discipline so that the inmates have a happy, enjoyable and memorable stay.</p>";
+
+  if(x.style.display=="none")
+  {
+  x.style.display="block";
+  }
+  else{
+    x.style.display="none";
+  }
+  
+}
+
